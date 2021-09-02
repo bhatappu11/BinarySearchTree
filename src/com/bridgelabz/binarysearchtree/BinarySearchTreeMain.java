d603c413222207a5ab561fd7126c563ac7775401
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-02 16:57:12 +0530

    ADDED: created bst of three nodes

--- src/com/bridgelabz/binarysearchtree/BinarySearchTreeMain.java
+++ src/com/bridgelabz/binarysearchtree/BinarySearchTreeMain.java
@@ -3,7 +3,12 @@ package com.bridgelabz.binarysearchtree;
 public class BinarySearchTreeMain {
 
 	public static void main(String[] args) {
-
+		MyBinaryTree<Integer> binaryTree = new MyBinaryTree<Integer>();
+		binaryTree.add(56);
+		binaryTree.add(30);
+		binaryTree.add(70);
+		int space = 0,height = 10;
+		binaryTree.printBinaryTree(binaryTree.getRoot(),space,height);
 	}
 
 }
---
+++ src/com/bridgelabz/binarysearchtree/MyBinaryTree.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.binarysearchtree;
+
+public class MyBinaryTree<K extends Comparable<K>> {
+	private MyBinaryNode<K> root;
+	
+	public void add(K key) {
+		this.root = this.addRecursively(root, key);
+	}
+	private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key) {
+		if(current == null)
+			return new MyBinaryNode<K>(key);
+		int compareResult = key.compareTo(current.key);
+		if(compareResult == 0) return current;
+		if(compareResult < 0) {
+			current.left = addRecursively(current.left, key);
+		} else {
+			current.right = addRecursively(current.right, key);
+		}
+		return current;
+	}
+	public MyBinaryNode<K> getRoot(){
+		return root;
+	}
+	public void printBinaryTree(MyBinaryNode<K> root,int space, int height)
+    {
+        if (root == null) {
+            return;
+        }
+        space += height;
+        printBinaryTree(root.right, space, height);
+        System.out.println();
+        for (int i = height; i < space; i++) {
+            System.out.print(' ');
+        }
+        System.out.print(root.key);
+        System.out.println();
+        printBinaryTree(root.left, space, height);
+    }
+ 
+}
+ 
